53d5c32bfee4a4a240fb9d3c786e67a85cb3886b
Arafath <alarafath.info@gmail.com>
2026-08-02 22:04:13 +0600

    solve a problem

---
+++ Codechef/Conquer the Fest!!.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n,b;
+    cin>>n>>b;
+    
+    if(n >= (b*10)) cout<<"YES\n";
+    else cout<<"NO\n";
+}
